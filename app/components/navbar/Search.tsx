5d3a55d8718f66a4e10ee64c3635908db0efa6f3
bluedream74 <bluedream7004@gmail.com>
2024-04-24 01:40:41 +0900

    navbar

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { BiSearch } from "react-icons/bi";
+
+const Search = () => {
+  return ( 
+    <div
+      className="
+        border-[1px]
+        w-full
+        md:w-auto
+        py-2
+        rounded-full
+        shadow-sm
+        hover:shadow-md
+        transition
+        cursor-pointer
+        bg-white
+      "
+    >
+      <div
+        className="
+          flex
+          flex-row
+          items-center
+          justify-between
+        "
+      >
+        <div
+          className="
+            text-sm
+            font-semibold
+            px-6
+          "
+        >
+          {/* <input type="text" /> */}
+        </div>
+        <div
+          className="
+            pr-2
+          "
+        >
+          <div
+            className="
+              p-2
+              bg-rose-500
+              rounded-full
+              text-white
+            "
+          >
+            <BiSearch size={18} />
+          </div>
+        </div>
+      </div>
+    </div>
+   );
+}
+ 
+export default Search;
